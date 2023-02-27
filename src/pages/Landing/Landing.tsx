f5d7ac6ece90c4c4995c61ca16eb534234f1b6f5
Izabela <izabelaszeremeta@gmail.com>
2023-02-27 08:42:47 -0600

    add images

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,5 +1,7 @@
 // stylesheets
-import logo from '../../assets/icons/coffeeBeans.png'
+import logo from '../../assets/icons/espresso.jpg'
+import icon from '../../assets/icons/cappucinos.jpg'
+import img from '../../assets/icons/latteArt.jpg'
 import styles from './Landing.module.css'
 
 // services
@@ -24,7 +26,9 @@ const Landing = (props: LandingProps): JSX.Element => {
   return (
     <main className={styles.container}>
       <h1>Coffee Shops</h1>
+      <img src={img} alt='flat' />
       <img src={logo} alt='beans' />
+      <img src={icon} alt='cap' />
       { user &&
         <button onClick={handleDeleteAccount}>
           DELETE ACCOUNT
